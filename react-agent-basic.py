27c5ade1fb6932aa5a6f093bf985e220503b5ce9
jshwanth <sjashwanth488@gmail.com>
2025-04-29 19:22:28 +0530

    first commit

---
+++ react-agent-basic.py
@@ -0,0 +1,16 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain.agents import initialize_agent
+from langchain_community.tools import TavilySearchResults
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+
+search_tool = TavilySearchResults(search_depth="basic")
+tools = [search_tool]
+
+agent = initialize_agent(tools =tools, llm=llm,agent="zero-shot-react-description", verbose=True)
+agent.invoke("Give me a funny fact about the Eiffel Tower")
+# result = llm.invoke("Give me a fact about the Eiffel Tower.")
+# print(result)
